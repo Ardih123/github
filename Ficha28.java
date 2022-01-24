2bb4027f1495b19fbcd7a36c10240b972edda72d
Ardih123 <joaoandrade888@gmail.com>
2022-01-24 15:24:56 +0000

    ok
    ok

---
+++ Ficha28.java
@@ -0,0 +1,36 @@
+package github;
+
+import java.util.Scanner;
+
+public class Ficha28 {
+//Escreva uma função chamada convertSeconds que recebe um número (segundos) e imprimeo númerode minutos, horas, dias e meses 
+	//(considere que um mês tem 30 dias).
+	
+	public static void convertSeconds(int segundos) {
+		int dias = segundos/(24*3600); 
+		  
+		segundos = segundos%(24*3600); 
+	    int horas = segundos/3600; 
+	  
+	    segundos %= 3600; 
+	    int minutos = segundos/60; 
+	  
+	    segundos %= 60; 
+	    int seg = segundos;
+	    
+	    System.out.println(dias +" dias, "+horas+" horas, "+minutos+" minutos, "+seg+" segundos");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in); 
+	    
+        System.out.println("escreva os segundos");  
+        int segundos = input.nextInt(); 
+        input.close();
+        
+        convertSeconds(segundos);
+	}
+
+}
